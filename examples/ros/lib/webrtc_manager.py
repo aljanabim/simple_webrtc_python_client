f474cc5b8881c00194f62c4fd5190b710974c304
aljanabim <ma970101@yahoo.se>
2021-08-16 13:17:22 +0200

    Fixed cv2 import error on Ubuntu 16.04 using ROS kinetic

--- examples/ros/lib/webrtc_manager.py
+++ examples/ros/lib/webrtc_manager.py
@@ -1,9 +1,12 @@
+import sys
+sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages') # in order to import cv2 under python3
+import cv2
+sys.path.append('/opt/ros/kinetic/lib/python2.7/dist-packages') # append back in order to import rospy
 import asyncio
 from aiortc import RTCIceCandidate, RTCPeerConnection, RTCSessionDescription, RTCConfiguration, RTCIceServer, VideoStreamTrack
 from aiortc.contrib.media import MediaPlayer, MediaRecorder, MediaRelay
 import platform
 from .media_handlers import StreamViewer
-# import cv2
 
 class WebrtcManager():
     """
