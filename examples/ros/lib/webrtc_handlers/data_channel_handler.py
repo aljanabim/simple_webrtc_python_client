c36dce60bbce647388183da79fcac9a557dedb43
aljanabim <ma970101@yahoo.se>
2021-08-27 17:42:50 +0200

    Added ping pong test for data channel and ros

--- examples/ros/lib/webrtc_handlers/data_channel_handler.py
+++ examples/ros/lib/webrtc_handlers/data_channel_handler.py
@@ -6,7 +6,7 @@ if platform.release() == '4.9.140-tegra':
     sys.path.remove('/usr/lib/python2.7/dist-packages') # append back in order to import rospy
 else:
     import rospy
-
+import time
 import asyncio
 import queue
 from svea_msgs.msg import lli_ctrl
@@ -48,8 +48,16 @@ def data_channel_handler(our_peer_id, our_peer_type, peers, peer_id):
     channel = peer.get('dataChannel')
     # topics_to_send = [Topic("chatter", String)]
 
-    rospy.Subscriber('/svea1/lli/ctrl_request', lli_ctrl, cb)
-    pub = rospy.Publisher(f"/{our_peer_id}/lli/ctrl_request", lli_ctrl, queue_size=1)
+    # rospy.Subscriber('/svea1/lli/ctrl_request', lli_ctrl, cb)
+    # pub = rospy.Publisher(f"/{our_peer_id}/lli/ctrl_request", lli_ctrl, queue_size=1)
+    ping = rospy.Publisher("ping", String)
+    def pong_cb(msg):
+        m = msg.data.split(" ")
+        start_time = float(m[-1])
+        idx = int(m[1])
+        print("Pong", idx, time.time()-start_time)
+    rospy.Subscriber('pong', String, pong_cb)
+
     # event_loop = asyncio.get_running_loop()
 
     # ros_handler = RosHandler(our_peer_id, peer_id, channel, asyncio.get_running_loop(), topics_to_send)
@@ -58,6 +66,10 @@ def data_channel_handler(our_peer_id, our_peer_type, peers, peer_id):
     def on_open():
         print("Data channel with", peer_id, "is open")
         # channel.send("Hello from {}".format(our_peer_id))
+        for i in range(100):
+            ping.publish("Ping" + " " + str(i) + " " + str(time.time()))
+            # channel.send("Ping" + " " + str(i) + " " + str(time.time()))
+
 
     @channel.on("close")
     def on_close():
@@ -65,12 +77,21 @@ def data_channel_handler(our_peer_id, our_peer_type, peers, peer_id):
 
     @channel.on("message")
     def on_message(message):
+        m = message.split(" ")
+        start_time = float(m[-1])
+        idx = int(m[1])
+        print("Pong", idx, time.time()-start_time)
+        if idx == 99:
+            input("Ping?")
+            for i in range(100):
+                channel.send("Ping" + " " + str(i) + " " + str(time.time()))
+        # channel.send("Ping " + str(time.time()))
         # print(message, message[0:4])
-        pub.publish(lli_ctrl().deserialize(message))
+        # pub.publish(lli_ctrl().deserialize(message))
         # print(lli_ctrl().deserialize(message))
         # print(peer_id, "says", message)
 
     # ros_handler._init_and_spin_ros()
     # rospy.Subscriber('/Right/chatter', String, lambda msg: cb(channel, ee, msg))
-    asyncio.Task(start(channel))
+    # asyncio.Task(start(channel))
 
